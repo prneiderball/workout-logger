ce138d26872180522b0baff719e8d8b9833ee403
prneiderball <phillrballn@icloud.com>
2026-02-16 14:24:51 -0600

    updated app.jsx

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -3,17 +3,25 @@ import "./App.css";
 import Header from "../Header/Header.jsx";
 
 function App() {
+  const Today = new Date().toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+  });
+
   return (
     <main className="app">
       <Header />
       <section className="app__content">
         <div className="app__date">
-          <p>Today: February 16th, 2026</p>
+          <time>Today: {Today}</time>
         </div>
 
-        <button className="app__btn">Log workout</button>
+        <button className="app__btn"> + Log workout</button>
 
-        <div className="app__history--container">Previous Workouts:</div>
+        <div className="app__history--container">
+          <h2 className="app__history--title">Previous Workouts:</h2>
+        </div>
       </section>
     </main>
   );
